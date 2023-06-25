e0050f99767cc95960ddf00ad11b8aeedc23c709
lukas <lukas.weber.4.12@gmail.com>
2023-06-25 19:29:33 +0200

    take complexity spirit demon out of project structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,110 @@
-pub use buffer::window::bgr::WindowBGRBuffer;
+use crate::wrappers::{CreatedHdcWrapper, HbitmapWrapper, HdcWrapper};
+use std::mem::size_of;
+use std::ops::Not;
+use windows::core::Error;
+use windows::Win32::Foundation::{ERROR_INVALID_PARAMETER, E_FAIL, HWND, RECT};
+use windows::Win32::Graphics::Gdi::{
+    GetDIBits, SelectObject, BITMAPINFO, BITMAPINFOHEADER, BI_RGB, DIB_RGB_COLORS,
+};
+use windows::Win32::Storage::Xps::{PrintWindow, PRINT_WINDOW_FLAGS};
+use windows::Win32::UI::HiDpi::{SetProcessDpiAwareness, PROCESS_PER_MONITOR_DPI_AWARE};
+use windows::Win32::UI::WindowsAndMessaging::{GetWindowRect, PW_RENDERFULLCONTENT};
 
-mod buffer;
+mod wrappers;
+
+pub struct WindowBGRBuffer {
+    handle: HWND,
+    width: i32,
+    height: i32,
+    hdc: CreatedHdcWrapper,
+    hbitmap: HbitmapWrapper,
+    buffer: Vec<u8>,
+}
+
+impl WindowBGRBuffer {
+    pub fn new(handle: HWND) -> windows::core::Result<Self> {
+        unsafe {
+            let _ = SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE);
+        }
+
+        let mut rect = RECT::default();
+        unsafe {
+            if GetWindowRect(handle, &mut rect).as_bool().not() {
+                return Err(Error::from_win32());
+            };
+        }
+        let width = rect.right - rect.left;
+        let height = rect.bottom - rect.top;
+
+        let hdc_screen = HdcWrapper::get_dc(handle)?;
+
+        let hdc = CreatedHdcWrapper::create_compatible_dc(hdc_screen.inner())?;
+        let hbitmap = HbitmapWrapper::create_compatible_bitmap(hdc_screen.inner(), width, height)?;
+
+        unsafe {
+            if SelectObject(hdc.inner(), hbitmap.inner()).is_invalid() {
+                return Err(Error::from_win32());
+            }
+        }
+
+        let flags = PRINT_WINDOW_FLAGS(PW_RENDERFULLCONTENT);
+        unsafe {
+            if PrintWindow(handle, hdc.inner(), flags) == false {
+                return Err(Error::from_win32());
+            }
+        }
+
+        Ok(Self {
+            handle,
+            width,
+            height,
+            hdc,
+            hbitmap,
+            buffer: vec![0; (4 * width * height) as usize],
+        })
+    }
+
+    pub fn height(&self) -> i32 {
+        self.height
+    }
+
+    pub fn width(&self) -> i32 {
+        self.width
+    }
+
+    pub fn buffer(&self) -> &Vec<u8> {
+        &self.buffer
+    }
+
+    pub fn read(&mut self) -> windows::core::Result<()> {
+        let bitmap_info_header = BITMAPINFOHEADER {
+            biSize: size_of::<BITMAPINFOHEADER>() as u32,
+            biPlanes: 1,
+            biBitCount: 32,
+            biWidth: self.width,
+            biHeight: -self.height,
+            biCompression: BI_RGB.0 as u32,
+            ..Default::default()
+        };
+        let mut bit_map_info = BITMAPINFO {
+            bmiHeader: bitmap_info_header,
+            ..Default::default()
+        };
+
+        unsafe {
+            let gdb = GetDIBits(
+                self.hdc.inner(),
+                self.hbitmap.inner(),
+                0,
+                self.height as u32,
+                Some(self.buffer.as_mut_ptr() as *mut core::ffi::c_void),
+                &mut bit_map_info.clone(),
+                DIB_RGB_COLORS,
+            );
+            if gdb == 0 || gdb == ERROR_INVALID_PARAMETER.0 as i32 {
+                return Err(Error::new(E_FAIL, "GetDIBits error".into()));
+            }
+            Ok(())
+        }
+    }
+}
---
+++ src/wrappers.rs
@@ -0,0 +1,95 @@
+use windows::{
+    core::Error,
+    Win32::{
+        Foundation::HWND,
+        Graphics::Gdi::{
+            CreateCompatibleBitmap, CreateCompatibleDC, CreatedHDC, DeleteDC, DeleteObject, GetDC,
+            ReleaseDC, HBITMAP, HDC,
+        },
+    },
+};
+
+pub(crate) struct HdcWrapper {
+    inner: HDC,
+}
+
+impl HdcWrapper {
+    pub(crate) fn get_dc(hwnd: HWND) -> Result<HdcWrapper, Error> {
+        unsafe {
+            match GetDC(hwnd) {
+                e if e.is_invalid() => Err(Error::from_win32()),
+                hdc => Ok(HdcWrapper { inner: hdc }),
+            }
+        }
+    }
+
+    pub(crate) fn inner(&self) -> HDC {
+        self.inner
+    }
+}
+
+impl Drop for HdcWrapper {
+    fn drop(&mut self) {
+        unsafe {
+            ReleaseDC(HWND::default(), self.inner);
+        }
+    }
+}
+
+pub(crate) struct CreatedHdcWrapper {
+    inner: CreatedHDC,
+}
+
+impl CreatedHdcWrapper {
+    pub(crate) fn create_compatible_dc(hdc: HDC) -> Result<CreatedHdcWrapper, Error> {
+        unsafe {
+            match CreateCompatibleDC(hdc) {
+                error if error.is_invalid() => Err(Error::from_win32()),
+                hdc => Ok(CreatedHdcWrapper { inner: hdc }),
+            }
+        }
+    }
+
+    pub(crate) fn inner(&self) -> HDC {
+        HDC(self.inner.0)
+    }
+}
+
+impl Drop for CreatedHdcWrapper {
+    fn drop(&mut self) {
+        unsafe {
+            DeleteDC(self.inner);
+        }
+    }
+}
+
+pub(crate) struct HbitmapWrapper {
+    inner: HBITMAP,
+}
+
+impl HbitmapWrapper {
+    pub(crate) fn create_compatible_bitmap(
+        hdc: HDC,
+        w: i32,
+        h: i32,
+    ) -> Result<HbitmapWrapper, Error> {
+        unsafe {
+            match CreateCompatibleBitmap(hdc, w, h) {
+                e if e.is_invalid() => Err(Error::from_win32()),
+                hbitmap => Ok(HbitmapWrapper { inner: hbitmap }),
+            }
+        }
+    }
+
+    pub(crate) fn inner(&self) -> HBITMAP {
+        self.inner
+    }
+}
+
+impl Drop for HbitmapWrapper {
+    fn drop(&mut self) {
+        unsafe {
+            DeleteObject(self.inner);
+        }
+    }
+}
