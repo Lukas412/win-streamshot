3a529d493890a0fef3ce41c98c5a83801040ea01
lukas <lukas.weber.4.12@gmail.com>
2023-06-26 20:17:31 +0200

    improve api

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,104 @@
 use {
   crate::wrappers::{CreatedHdcWrapper, HbitmapWrapper, HdcWrapper},
-  std::{mem::size_of, ops::Not},
+  std::{
+    mem::size_of,
+    ops::{Deref, Not},
+  },
   windows::{
     core::Error,
     Win32::{
-      Foundation::{ERROR_INVALID_PARAMETER, E_FAIL, HWND, RECT},
+      Foundation::{BOOL, ERROR_INVALID_PARAMETER, E_FAIL, HWND, LPARAM, RECT},
       Graphics::Gdi::{
         GetDIBits, SelectObject, BITMAPINFO, BITMAPINFOHEADER, BI_RGB, DIB_RGB_COLORS,
       },
       Storage::Xps::{PrintWindow, PRINT_WINDOW_FLAGS},
       UI::{
         HiDpi::{SetProcessDpiAwareness, PROCESS_PER_MONITOR_DPI_AWARE},
-        WindowsAndMessaging::{GetWindowRect, PW_RENDERFULLCONTENT},
+        WindowsAndMessaging::{
+          EnumWindows, GetWindowRect, GetWindowTextLengthW, GetWindowTextW, IsWindowVisible,
+          PW_RENDERFULLCONTENT,
+        },
       },
     },
   },
 };
+
+pub struct WindowFinder {
+  windows: Vec<Window>,
+}
+
+impl WindowFinder {
+  pub fn new() -> windows::core::Result<Self> {
+    Ok(Self {
+      windows: get_windows()?,
+    })
+  }
+
+  pub fn find(&self, name: &str) -> Option<WindowScreenshotBuffer> {
+    self
+      .windows
+      .iter()
+      .filter(|window| window.name.contains(name))
+      .next()
+      .map(WindowScreenshotBuffer::new)
+  }
+
+  pub fn find_exact(&self, name: &str) -> Option<WindowScreenshotBuffer> {
+    self
+      .windows
+      .iter()
+      .filter(|window| window.name == name)
+      .next()
+      .map(WindowScreenshotBuffer::new)
+  }
+}
+
+struct Window {
+  handle: HWND,
+  name: String,
+}
+
+pub fn get_windows() -> windows::core::Result<Vec<Window>> {
+  let mut windows = Vec::new();
+  unsafe {
+    let result = EnumWindows(
+      Some(wl_callback),
+      LPARAM(&mut windows as *mut Vec<HwndName> as isize),
+    );
+    if result == false {
+      return Err(Error::from_win32());
+    }
+  }
+  Ok(windows)
+}
+
+unsafe extern "system" fn wl_callback(hwnd: HWND, lparam: LPARAM) -> BOOL {
+  let windows = lparam.0 as *mut Vec<Window>;
+
+  if IsWindowVisible(hwnd) == false {
+    return BOOL::from(true);
+  }
+
+  let window_text_length = GetWindowTextLengthW(hwnd);
+  if window_text_length == 0 {
+    return BOOL::from(true);
+  }
+
+  let mut name_buf: Vec<u16> = vec![0; (window_text_length + 1) as usize];
+  if GetWindowTextW(hwnd, &mut name_buf) == 0 {
+    return BOOL::from(true);
+  }
+
+  let name_buf = match name_buf.split_last() {
+    Some((_, last)) => last,
+    None => return BOOL::from(true),
+  };
+
+  let name = String::from_utf16_lossy(name_buf);
+  (*windows).push(Window { handle: hwnd, name });
+
+  BOOL::from(true)
+}
 
 mod wrappers;
 
@@ -23,8 +106,6 @@ pub struct WindowScreenshotBuffer {
   handle: HWND,
   width: i32,
   height: i32,
-  hdc: CreatedHdcWrapper,
-  hbitmap: HbitmapWrapper,
   buffer: Vec<u8>,
 }
 
@@ -43,6 +124,37 @@ impl WindowScreenshotBuffer {
     let width = rect.right - rect.left;
     let height = rect.bottom - rect.top;
 
+    Ok(Self {
+      handle,
+      width,
+      height,
+      buffer: vec![0; (4 * width * height) as usize],
+    })
+  }
+
+  pub fn get_bgr_screenshot(&mut self) -> windows::core::Result<Screenshot<BGRA>> {
+    self.read()?;
+    Ok(Screenshot {
+      width: self.width as u32,
+      height: self.height as u32,
+      image: &self.buffer,
+    })
+  }
+
+  pub fn get_rgb_screenshot(&mut self) -> windows::core::Result<Screenshot<RGBA>> {
+    self.read()?;
+    self
+      .buffer
+      .chunks_exact_mut(4)
+      .for_each(|pixel| pixel.swap(0, 2));
+    Ok(Screenshot {
+      width: self.width as u32,
+      height: self.height as u32,
+      image: &self.buffer,
+    })
+  }
+
+  fn read(&mut self) -> windows::core::Result<()> {
     let hdc_screen = HdcWrapper::get_dc(handle)?;
 
     let hdc = CreatedHdcWrapper::create_compatible_dc(hdc_screen.inner())?;
@@ -54,36 +166,6 @@ impl WindowScreenshotBuffer {
       }
     }
 
-    let flags = PRINT_WINDOW_FLAGS(PW_RENDERFULLCONTENT);
-    unsafe {
-      if PrintWindow(handle, hdc.inner(), flags) == false {
-        return Err(Error::from_win32());
-      }
-    }
-
-    Ok(Self {
-      handle,
-      width,
-      height,
-      hdc,
-      hbitmap,
-      buffer: vec![0; (4 * width * height) as usize],
-    })
-  }
-
-  pub fn height(&self) -> i32 {
-    self.height
-  }
-
-  pub fn width(&self) -> i32 {
-    self.width
-  }
-
-  pub fn buffer(&self) -> &Vec<u8> {
-    &self.buffer
-  }
-
-  pub fn read(&mut self) -> windows::core::Result<()> {
     let bitmap_info_header = BITMAPINFOHEADER {
       biSize: size_of::<BITMAPINFOHEADER>() as u32,
       biPlanes: 1,
@@ -111,7 +193,34 @@ impl WindowScreenshotBuffer {
       if gdb == 0 || gdb == ERROR_INVALID_PARAMETER.0 as i32 {
         return Err(Error::new(E_FAIL, "GetDIBits error".into()));
       }
-      Ok(())
     }
+    Ok(())
+  }
+}
+
+struct BGRA;
+struct RGBA;
+
+pub struct Screenshot<'a, Color> {
+  width: u32,
+  height: u32,
+  image: &'a Vec<u8>,
+}
+
+impl<'a, Color> Screenshot<'a, Color> {
+  pub fn width(&self) -> u32 {
+    self.width
+  }
+
+  pub fn height(&self) -> u32 {
+    self.height
+  }
+}
+
+impl<'a, Color> Deref for Screenshot<'a, Color> {
+  type Target = &'a Vec<u8>;
+
+  fn deref(&self) -> &Self::Target {
+    &self.image
   }
 }
